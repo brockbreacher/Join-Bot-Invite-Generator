1c4c196aee198e9f32fb7bc07d8ebfda519b9e59
KindKid27 <kindkid27@pm.me>
2025-04-10 12:19:25 -0500

    Add permission customization
    I really find this tool useful, but it lacked permission customization, so I added it :D
    I hope this PR get's accepted :>

--- script.js
+++ script.js
@@ -7,10 +7,32 @@ function updateYear() {
 // Call the function to update the year when the script loads
 updateYear();
 
+// set the permissions value from the # of the url when the page laods
+var hash = window.location.hash;
+if (hash.length && !isNaN(hash = parseInt(hash.substr(1)))) {
+    document.getElementById('permissions').value = hash;
+}
+
+// update the # of the url when the permissions input changes
+document.getElementById('permissions').addEventListener('input', function() {
+    var permissions = document.getElementById('permissions').value;
+    if (permissions.length) {
+        window.location.hash = permissions;
+    } else {
+        window.location.hash = '';
+    }
+});
+
 // Function to generate the invite link
 document.getElementById('generateBtn').addEventListener('click', function() {
+    
     const clientId = document.getElementById('clientId').value;
-    const inviteLink = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=2147503232&scope=bot%20applications.commands`;
+    const permissions = document.getElementById('permissions').value;
+    if (!clientId) {
+        alert('please enter a Client ID');
+        return;
+    }
+    const inviteLink = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&permissions=${permissions||0}&scope=bot%20applications.commands`;
     document.getElementById('inviteLink').href = inviteLink;
     document.getElementById('inviteLink').textContent = inviteLink;
     document.getElementById('generatedLink').style.display = 'block'; // Ensure this line is present to show the generated link container
